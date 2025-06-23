e5c8ee4c6c7cf6928a7c026d6b95c5ce89591eed
Rai <railynmagondaya@gmail.com>
2025-06-24 00:08:52 +0800

    Combined duplicate code, removed repeated image declarations, enhancing while simplifying the code further.

--- jakes_sandwich_chase.py
+++ jakes_sandwich_chase.py
@@ -8,8 +8,6 @@ GAME_HEIGHT = 600
 SPEED = 150
 SPACE_SIZE = 50
 BODY_PARTS = 3
-SNAKE_COLOR = "#00FF00"
-FOOD_COLOR = "#FF0000"
 BACKGROUND_COLOR = "#000000"
 
 class GameObject:
@@ -37,58 +35,45 @@ class Jake(GameObject):
         self.body_size = BODY_PARTS
         self.coordinates = [[0, 0] for _ in range(self.body_size)]
         self.images = []
-        self.current_direction = 'down'
+        self.direction = 'down'
 
-        self.head_images = ImageLoader.load_rotated_images("assets/jake_head.png")
-        self.body_images = ImageLoader.load_rotated_images("assets/jake_body.png")
-        self.tail_images = ImageLoader.load_rotated_images("assets/jake_bottom.png")
+        self.head = ImageLoader.load_rotated_images("assets/jake_head.png")
+        self.body = ImageLoader.load_rotated_images("assets/jake_body.png")
+        self.tail = ImageLoader.load_rotated_images("assets/jake_bottom.png")
 
         for index, (x_axis, y_axis) in enumerate(self.coordinates):
-            image = self._get_body_part_image(index)
-            image_id = canvas.create_image(x_axis, y_axis, image=image, anchor="nw", tag="snake")
-            self.images.append(image_id)
-
-    def _get_body_part_image(self, index):
-        if index == 0:
-            return self.head_images['right']
-        elif index == self.body_size - 1:
-            return self.tail_images['right']
-        return self.body_images['right']
+            part_image = self.head['right'] if index == 0 else self.tail['right'] if index == self.body_size - 1 else self.body['right']
+            self.images.append(canvas.create_image(x_axis, y_axis, image=part_image, anchor="nw", tag="snake"))
 
     def set_direction(self, new_direction):
-        self.current_direction = new_direction
+        self.direction = new_direction
 
     def grow(self, x_axis, y_axis):
         self.coordinates.insert(0, (x_axis, y_axis))
-        head_image = self.head_images[self.current_direction]
-        image_id = self.canvas.create_image(x_axis, y_axis, image=head_image, anchor="nw", tag="snake")
-        self.images.insert(0, image_id)
-
+        self.images.insert(0, self.canvas.create_image(x_axis, y_axis, image=self.head[self.direction], anchor="nw", tag="snake"))
         if len(self.images) > 1:
-            self.canvas.itemconfig(self.images[1], image=self.body_images[self.current_direction])
+            self.canvas.itemconfig(self.images[1], image=self.body[self.direction])
         if len(self.images) >= 2:
-            self._update_tail_image()
+            self._update_tail()
 
     def shrink(self):
         self.coordinates.pop()
         self.canvas.delete(self.images.pop())
         if len(self.coordinates) >= 2:
-            self._update_tail_image()
+            self._update_tail()
 
-    def _update_tail_image(self):
-        second_to_last_x, second_to_last_y = self.coordinates[-2]
-        last_x, last_y = self.coordinates[-1]
-
-        if second_to_last_x > last_x:
+    def _update_tail(self):
+        second_to_last_x_axis, second_to_last_y_axis = self.coordinates[-2]
+        last_x_axis, last_y_axis = self.coordinates[-1]
+        if second_to_last_x_axis > last_x_axis:
             tail_direction = 'right'
-        elif second_to_last_x < last_x:
+        elif second_to_last_x_axis < last_x_axis:
             tail_direction = 'left'
-        elif second_to_last_y > last_y:
+        elif second_to_last_y_axis > last_y_axis:
             tail_direction = 'down'
         else:
             tail_direction = 'up'
-
-        self.canvas.itemconfig(self.images[-1], image=self.tail_images[tail_direction])
+        self.canvas.itemconfig(self.images[-1], image=self.tail[tail_direction])
 
 class Food(GameObject):
     def __init__(self, canvas):
@@ -101,21 +86,17 @@ class Food(GameObject):
 
 def next_turn():
     global score, direction, food
-
-    head_x_axis, head_y_axis = jake.coordinates[0]
-    move_map = {'up': (0, -SPACE_SIZE), 'down': (0, SPACE_SIZE), 'left': (-SPACE_SIZE, 0), 'right': (SPACE_SIZE, 0)}
-    delta_x, delta_y = move_map[direction]
-    new_x_axis = head_x_axis + delta_x
-    new_y_axis = head_y_axis + delta_y
-
-    jake.grow(new_x_axis, new_y_axis)
+    delta_x_axis, delta_y_axis = {'up': (0, -SPACE_SIZE), 'down': (0, SPACE_SIZE), 'left': (-SPACE_SIZE, 0), 'right': (SPACE_SIZE, 0)}[direction]
+    current_head_x_axis, current_head_y_axis = jake.coordinates[0]
+    new_head_x_axis = current_head_x_axis + delta_x_axis
+    new_head_y_axis = current_head_y_axis + delta_y_axis
+    jake.grow(new_head_x_axis, new_head_y_axis)
 
     food_x_axis, food_y_axis = food.get_position()
-
-    if new_x_axis == food_x_axis and new_y_axis == food_y_axis:
+    if (new_head_x_axis, new_head_y_axis) == (food_x_axis, food_y_axis):
         eat_sound.play()
         score += 1
-        label.config(text=f"Score:{score}")
+        label.config(text=f"Score: {score}")
         canvas.delete("food")
         food = Food(canvas)
     else:
@@ -128,30 +109,24 @@ def next_turn():
 
 def change_direction(new_direction):
     global direction
-    if direction != {'up': 'down', 'down': 'up', 'left': 'right', 'right': 'left'}.get(new_direction):
+    if direction != {'up': 'down', 'down': 'up', 'left': 'right', 'right': 'left'}[new_direction]:
         direction = new_direction
         jake.set_direction(new_direction)
 
 def check_collisions():
-    x_axis, y_axis = jake.coordinates[0]
+    head_x_axis, head_y_axis = jake.coordinates[0]
     return (
-        x_axis < 0 or x_axis >= GAME_WIDTH or y_axis < 0 or y_axis >= GAME_HEIGHT or
-        (x_axis, y_axis) in jake.coordinates[1:]
+        head_x_axis < 0 or head_x_axis >= GAME_WIDTH or head_y_axis < 0 or head_y_axis >= GAME_HEIGHT or
+        (head_x_axis, head_y_axis) in jake.coordinates[1:]
     )
 
 def game_over():
     canvas.delete(ALL)
-    game_over_background = PhotoImage(file="assets/jake_ending.png")
-    canvas.create_image(0, 0, image=game_over_background, anchor="nw", tag="gameover_bg")
-    canvas.game_over_background = game_over_background
-    canvas.create_text(GAME_WIDTH / 2, GAME_HEIGHT / 2,
-                       font=('consolas', 70), text="GAME OVER", fill="red", tag="gameover_text")
-
+    canvas.game_over_background_image = PhotoImage(file="assets/jake_ending.png")
+    canvas.create_image(0, 0, image=canvas.game_over_background_image, anchor="nw", tag="gameover_bg")
+    canvas.create_text(GAME_WIDTH / 2, GAME_HEIGHT / 2, font=('consolas', 70), text="GAME OVER", fill="red", tag="gameover_text")
     pygame.mixer.music.stop()
-
-    scream_sound = pygame.mixer.Sound("assets/jake_scream_sound.wav")
-    scream_sound.play()
-
+    pygame.mixer.Sound("assets/jake_scream_sound.wav").play()
     pygame.mixer.music.load("assets/ending_bg_music.wav")
     pygame.mixer.music.play(-1)
 
@@ -159,24 +134,19 @@ window = Tk()
 window.title("Jake's Sandwich Chase")
 window.resizable(False, False)
 
-score = 0
-direction = 'down'
-
-label = Label(window, text=f"Score:{score}", font=('consolas', 40))
+score, direction = 0, 'down'
+label = Label(window, text="Score: 0", font=('consolas', 40))
 label.pack()
-
 canvas = Canvas(window, bg=BACKGROUND_COLOR, height=GAME_HEIGHT, width=GAME_WIDTH)
 canvas.pack()
-
-background_image = PhotoImage(file="assets/grass_bg.png")
-canvas.create_image(0, 0, image=background_image, anchor="nw")
-canvas.background = background_image
+canvas.background_image = PhotoImage(file="assets/grass_bg.png")
+canvas.create_image(0, 0, image=canvas.background_image, anchor="nw")
 
 window.update()
-window.geometry(f"{GAME_WIDTH}x{GAME_HEIGHT + label.winfo_height()}+{int((window.winfo_screenwidth() / 2) - (window.winfo_width() / 2))}+{int((window.winfo_screenheight() / 2) - (window.winfo_height() / 2))}")
+window.geometry(f"{GAME_WIDTH}x{GAME_HEIGHT + label.winfo_height()}+{int((window.winfo_screenwidth()/2) - (GAME_WIDTH/2))}+{int((window.winfo_screenheight()/2) - (GAME_HEIGHT/2))}")
 
-for key, direction_key in {'<Left>': 'left', '<Right>': 'right', '<Up>': 'up', '<Down>': 'down'}.items():
-    window.bind(key, lambda event, d=direction_key: change_direction(d))
+for key, dir_key in {'<Left>': 'left', '<Right>': 'right', '<Up>': 'up', '<Down>': 'down'}.items():
+    window.bind(key, lambda e, d=dir_key: change_direction(d))
 
 jake = Jake(canvas)
 food = Food(canvas)
@@ -184,7 +154,6 @@ food = Food(canvas)
 pygame.mixer.init()
 pygame.mixer.music.load("assets/adventure_time_bg_music.wav")
 pygame.mixer.music.play(-1)
-
 eat_sound = pygame.mixer.Sound("assets/eat_sound.wav")
 
 next_turn()
